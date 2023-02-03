632f1cc036101b463f767c7dac042c57d4309134
Raphael Costa <raphael.costa@sensedia.com>
2023-02-02 21:11:10 -0300

    add product box

--- src/components/product_card.js
+++ src/components/product_card.js
@@ -3,7 +3,7 @@ export default function Product_card() {
         <div className="p-2 bg-slate-50 w-fit mr-2 mt-3 flex flex-col justify-center p-3 rounded-lg shadow-lg">
             <img src={"https://picsum.photos/200"} />
             <div className="font-sans font-bold text-lime-800 mt-1">Dread de agulha</div>
-            <div className="text-sm font-sans mt-2">Dread feito de cabelo e tal e tudo mais</div>
+            <div className="text-sm font-sans text-black mt-2">Dread feito de cabelo e tal e tudo mais</div>
             <div className="mt-4">
                 <button className="bg-lime-800 font-bold text-white p-2 rounded-lg">
                     R$ 14.90
